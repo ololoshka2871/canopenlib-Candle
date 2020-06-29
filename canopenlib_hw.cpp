0609c2e5b1d38ccb9e18903c0246999f31d63151
tolyan <tolyan@sctbelpa>
2020-06-29 09:42:22 +0300

    open + close

--- canopenlib_hw.cpp
+++ canopenlib_hw.cpp
@@ -12,6 +12,15 @@
 #include <conio.h>
 #include <shlwapi.h>
 
+#include <vector>
+#include <memory>
+
+#include "candle.h"
+
+extern "C" {
+#include "candle_ctrl_req.h"
+}
+
 #ifdef _MANAGED
 #pragma managed(push, off)
 #endif
@@ -19,6 +28,60 @@
 #ifdef _MANAGED
 #pragma managed(pop)
 #endif
+
+struct py_candle_device;
+
+struct py_candle_channel {
+	py_candle_channel(py_candle_device& owner, uint8_t chanel = 0)
+		: owner{ owner }, _ch{ chanel }, echo_on{false} {	}
+
+	candle_capability_t capabilities() const {
+		candle_capability_t res;
+
+		candle_channel_get_capabilities(&owner, _ch, &res);
+
+		return res;
+	}
+
+	uint8_t chanel() const { return _ch; }
+
+	void setEchoEnabled(bool echoOn) {
+		echo_on = echoOn;
+	}
+
+	bool isEcho() const { return echo_on; }
+
+private:
+	py_candle_device& owner;
+	uint8_t _ch;
+	bool echo_on;
+
+	// RX FIFO
+	//struct fifo_t* _fifo;
+};
+
+struct py_candle_device {
+	py_candle_device(candle_handle dev)
+		: _handle(dev), _channel{ std::make_unique<py_candle_channel>(*this) } {}
+
+	void start_rx_thread() {}
+
+	candle_handle handle() const { return _handle; }
+	py_candle_channel& chanel() const { return *_channel; }
+
+	candle_capability_t capabilities() const { return _channel->capabilities(); }
+
+private:
+	// Candle device handle
+	candle_handle _handle;
+
+	// Open channels
+	std::unique_ptr<py_candle_channel> _channel;
+
+	// RX thread
+	HANDLE _rx_thread;
+	bool _rx_thread_stop_req;
+};
 
 BOOL APIENTRY DllMain(HMODULE hModule,
 	DWORD  ul_reason_for_call,
@@ -40,12 +103,36 @@ BOOL APIENTRY DllMain(HMODULE hModule,
 }
 
 //------------------------------------------------------------------------
+
+static std::vector<py_candle_device> py_candle_driver_list_devices()
+{
+	std::vector<py_candle_device> result;
+
+	candle_list_handle clist;
+	uint8_t num_devices;
+
+	if (candle_list_scan(&clist)) {
+		if (candle_list_length(clist, &num_devices)) {
+			for (uint8_t i = 0; i < num_devices; ++i) {
+				candle_handle dev;
+				if (candle_dev_get(clist, i, &dev)) {
+					result.emplace_back(dev);
+				}
+			}
+		}
+		candle_list_free(clist);
+	}
+	return result;
+}
+
+//------------------------------------------------------------------------
 //
 //------------------------------------------------------------------------
 
 CANOPENLIB_HW_API   canOpenStatus    __stdcall canPortLibraryInit(void)
 {
-	return CANOPEN_ERROR;
+	// prepare plugin to work
+	return CANOPEN_OK;
 }
 
 //------------------------------------------------------------------------
@@ -54,7 +141,41 @@ CANOPENLIB_HW_API   canOpenStatus    __stdcall canPortLibraryInit(void)
 
 CANOPENLIB_HW_API   canOpenStatus    __stdcall canPortOpen(int port, canPortHandle* handle)
 {
-	return CANOPEN_ERROR;
+	// open device and write it's handle to "handle"
+	auto devices = py_candle_driver_list_devices();
+	if (port >= devices.size()) {
+		return CANOPEN_OUT_OF_CAN_INTERFACES;
+	}
+
+	auto deviceOpening = new py_candle_device{ std::move(devices.at(port)) };
+
+#if 0
+	{
+		auto capabilities = deviceOpening->capabilities();
+		printf(R"V0G0N(feature = %d;
+		 fclk_can = %d;
+		 tseg1_min = %d;
+		 tseg1_max = %d;
+		 tseg2_min = %d;
+		 tseg2_max = %d;
+		 sjw_max = %d;
+		 brp_min = %d;
+		 brp_max = %d;
+		 brp_inc = %d;\n)V0G0N", capabilities.feature, capabilities.fclk_can, capabilities.tseg1_min, capabilities.tseg1_max,
+			capabilities.tseg2_min, capabilities.tseg2_max, capabilities.sjw_max, capabilities.brp_min,
+			capabilities.brp_max, capabilities.brp_inc);
+	}
+#endif
+
+	if (!candle_dev_open(deviceOpening->handle())) {
+		return CANOPEN_ERROR_HW_NOT_CONNECTED;
+	}
+
+	deviceOpening->start_rx_thread();
+
+	*handle = reinterpret_cast<canPortHandle>(deviceOpening);
+
+	return CANOPEN_OK;
 }
 
 //------------------------------------------------------------------------
@@ -63,7 +184,14 @@ CANOPENLIB_HW_API   canOpenStatus    __stdcall canPortOpen(int port, canPortHand
 
 CANOPENLIB_HW_API   canOpenStatus    __stdcall canPortClose(canPortHandle handle)
 {
-	return CANOPEN_ERROR;
+	// close device with "handle" provided
+	auto h = reinterpret_cast<py_candle_device*>(handle);
+
+	auto res = candle_dev_close(h->handle()) ? CANOPEN_OK : CANOPEN_ERROR;
+
+	candle_dev_free(h->handle());
+
+	return res;
 }
 
 //------------------------------------------------------------------------
@@ -72,7 +200,12 @@ CANOPENLIB_HW_API   canOpenStatus    __stdcall canPortClose(canPortHandle handle
 
 CANOPENLIB_HW_API   canOpenStatus    __stdcall canPortBitrateSet(canPortHandle handle, int bitrate)
 {
-	return CANOPEN_ERROR;
+	// set device with "hande" new "bitrate"
+	auto h = reinterpret_cast<py_candle_device*>(handle);
+
+	return candle_channel_set_bitrate(h->handle(), h->chanel().chanel(), bitrate)
+		? CANOPEN_OK
+		: CANOPEN_UNSUPPORTED_BITRATE;
 }
 
 //------------------------------------------------------------------------
@@ -81,7 +214,12 @@ CANOPENLIB_HW_API   canOpenStatus    __stdcall canPortBitrateSet(canPortHandle h
 
 CANOPENLIB_HW_API   canOpenStatus    __stdcall canPortEcho(canPortHandle handle, bool enabled)
 {
-	return CANOPEN_ERROR;
+	// set device with "hande" echo "enabled"
+	auto h = reinterpret_cast<py_candle_device*>(handle);
+
+	h->chanel().setEchoEnabled(enabled);
+
+	return CANOPEN_OK;
 }
 
 //------------------------------------------------------------------------
@@ -90,7 +228,19 @@ CANOPENLIB_HW_API   canOpenStatus    __stdcall canPortEcho(canPortHandle handle,
 
 CANOPENLIB_HW_API   canOpenStatus    __stdcall canPortGoBusOn(canPortHandle handle)
 {
-	return CANOPEN_ERROR;
+	// set device with "hande" enabled
+	auto h = reinterpret_cast<py_candle_device*>(handle);
+
+	int mode = CANDLE_MODE_NORMAL;
+
+	if (h->chanel().isEcho()) {
+		mode |= CANDLE_MODE_LOOP_BACK;
+	}
+
+	return candle_channel_start(h->handle(),
+		h->chanel().chanel(),
+		mode
+	) ? CANOPEN_OK : CANOPEN_ERROR;
 }
 
 //------------------------------------------------------------------------
@@ -99,7 +249,12 @@ CANOPENLIB_HW_API   canOpenStatus    __stdcall canPortGoBusOn(canPortHandle hand
 
 CANOPENLIB_HW_API   canOpenStatus    __stdcall canPortGoBusOff(canPortHandle handle)
 {
-	return CANOPEN_ERROR;
+	// set device with "hande" disabled
+	auto h = reinterpret_cast<py_candle_device*>(handle); 
+
+	return candle_channel_stop(h->handle(),
+		h->chanel().chanel()
+	) ? CANOPEN_OK : CANOPEN_ERROR;
 }
 
 //------------------------------------------------------------------------
@@ -135,5 +290,14 @@ CANOPENLIB_HW_API   canOpenStatus    __stdcall canPortRead(canPortHandle handle,
 CANOPENLIB_HW_API   canOpenStatus  __stdcall canPortGetSerialNumber(canPortHandle handle,
 	char* buffer, int bufferLen)
 {
-	return CANOPEN_ERROR;
+	candle_device_config_t dconf;
+	auto h = reinterpret_cast<py_candle_device*>(handle);
+
+	if (!candle_ctrl_get_config(static_cast<candle_device_t*>(h->handle()), &dconf)) {
+		return CANOPEN_ERROR;
+	}
+		
+	snprintf(buffer, bufferLen, "%d", dconf.sw_version);
+
+	return CANOPEN_OK;
 }
